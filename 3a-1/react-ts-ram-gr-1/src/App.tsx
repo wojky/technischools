448bf1b3f037375e8858d46026a056ead4251251
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-11-07 09:15:27 +0100

    group 1 class resources

--- 3a-1/react-ts-ram-gr-1/src/App.tsx
+++ 3a-1/react-ts-ram-gr-1/src/App.tsx
@@ -1,9 +1,29 @@
 import "./App.css";
+import React from "react";
 import { CharacterListItem } from "./CharacterListItem";
 
 type Character = { name: string; id: number };
 
 function App() {
+  const characterStatusList = [
+    {
+      innerText: "All",
+      value: "",
+    },
+    {
+      innerText: "Dead",
+      value: "dead",
+    },
+    {
+      innerText: "Alive",
+      value: "alive",
+    },
+    {
+      innerText: "Unknown",
+      value: "unknown",
+    },
+  ];
+
   const characters: Character[] = [
     {
       id: 1,
@@ -15,12 +35,37 @@ function App() {
     },
   ];
 
+  // return React.createElement("main", null, [
+  //   React.createElement("h1", { className: "" }, "Rick and Morty"),
+  //   React.createElement("input", null, null),
+  //   React.createElement('span',null, null),
+  //   React.createElement("button", { onClick: () => {} }, "Search"),
+  //   React.createElement(
+  //     "section",
+  //     null,
+  //     React.createElement(
+  //       "ol",
+  //       null,
+  //       characters.map(({ name, id }) => {
+  //         return React.createElement(CharacterListItem, { key: id, name });
+  //       })
+  //     )
+  //   ),
+  // ]);
+
   return (
     <main>
-      <h1>Rick and Morty</h1>
-
-      <input />
+      <h1 className="">Rick and Morty</h1>
+      {/* <label htmlFor=""></label> */}
+      <input placeholder="Search..." />
       <button onClick={() => {}}>Search</button>
+      <select>
+        {characterStatusList.map((status) => (
+          <option key={status.value} value={status.value}>
+            {status.innerText}
+          </option>
+        ))}
+      </select>
 
       <section>
         <ol>
